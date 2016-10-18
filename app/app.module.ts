429f204474de92141a70f9a6c7b197ec035cf4da
Dean Anthoney <moneymint23@yahoo.com>
2016-10-18 09:21:22 -0400

    Add files via upload

---
+++ app/app.module.ts
@@ -0,0 +1,19 @@
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }   from '@angular/forms';
+
+import { AppComponent }  from './app.component';
+import { AppHome } from './home';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  declarations: [
+    AppComponent,
+    AppHome
+  ],
+  bootstrap: [ AppComponent ]
+})
+export class AppModule { }
